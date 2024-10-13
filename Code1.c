8e0968767f4612b11bf5adf4991816fb495097e4
lpalmieriku <l250p415@home.ku.edu>
2024-10-13 01:24:57 -0500

    Finished commenting and also added user input
    Added user input to ask the user the name of the input file they would like the program to read from. I am also uploading the sample input file that I used to test the program.

--- Code1.c
+++ Code1.c
@@ -3,26 +3,31 @@ Author: Lena Palmieri
 KUID: 3115099
 Date: 10/9/24
 Lab: lab05
-Last Modified: 
-Purpose: 
+Last Modified: 10/12/24
+Purpose: Program reads a file input for the sales of 12 months and prints the statistics of those sales. 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//Function that prints all of the sales for each month
 void printSales(float nums[12], char months[12][10]) {
     int i = 0;
     printf("Monthly Sales Report for 2024\n\nMonth    \tSales\n");
+    //Loops through each index for every month and prints out the corresponding sales and month from each array
     while (i < 12) {
         printf("%s\t$%.2f\n", months[i], nums[i]);
         i++;
     }
 }
 
+//Function that prints out the minimum, maximum, and average sales for the year
 void printSalesReport(float nums[12], char months[12][10]) {
     int i = 0, minpos = 0, maxpos = 0;
     float min = nums[0], max = nums[0], average = 0;
+    //Loops through each index for every month and checks whether that value is the minimum or maximum for the year
+    //If it is, it stores both the value and position of that month
     while (i < 12) {
         if (min > nums[i]) {
             min = nums[i];
@@ -32,6 +37,7 @@ void printSalesReport(float nums[12], char months[12][10]) {
             max = nums[i];
             maxpos = i;
         }
+        //Adds each of the months sale's to the average and then divides it by 12 to get the average sales of the year
         average += nums[i];
         i++;
     }
@@ -39,14 +45,18 @@ void printSalesReport(float nums[12], char months[12][10]) {
     printf("\nSales summary report:\n\nMinimum Sales: $%.2f in %s\nMaximum Sales: $%.2f in %s\nAverage Sales: $%.2f\n", min, months[minpos], max, months[maxpos], average);
 }
 
+
+//Function that prints out the six-month average report for each six-month interval of the year
 void printSixMonths(float nums[12]) {
     printf("\nSix-month moving average report:\n\n");
+    //Loops through the 6 six-month intervals calculating the average throughout the six months
     for(int i = 0; i <= 6; i++) {
         float average = 0;
         for(int n = i; n < i + 6; n++) {
             average += nums[n];
         }
 
+        //Checks which six-month interval it is and prints out the corresponding month range and average
         if (i == 0) {
             printf("January-June\t$%.2f\n", average/6);
         }
@@ -71,18 +81,22 @@ void printSixMonths(float nums[12]) {
     }
 }
 
+//Function that prints out the month sales in order from highest to lowest
 void printHighLow(float nums[12], char months[12][10]) {
     printf("Sales report (highest to lowest)\n\nMonth    \tSales\n");
     int count = 0;
+    //Loops through each of the 12 months, checking for the maximum value in the list
     while (count < 12) {
         float max = nums[0];
         int maxpos = 0;
         for (int x = 0; x < 12; x++) {
+            //Once max value is found, it and its position is stored 
             if (max < nums[x]) {
                 max = nums[x];
                 maxpos = x;
             }
         }
+        //Once the next highest sale has been found, the value in that position of the array is set to -1 and the maximum sale and its corresponding month are printed
         nums[maxpos] = -1;
         count++;
         printf("%s\t$%.2f\n", months[maxpos], max);
@@ -90,19 +104,24 @@ void printHighLow(float nums[12], char months[12][10]) {
 }
 
 int main() {
-    char line[20];
+    char line[20], filename[100];
     float nums[12];
     char months[12][10] = {"January  ", "February", "March    ", "April    ", "May     ", "June    ", "July    ", "August   ", "September", "October ", "November ", "December "};
+    //Initializes the pointer value for the file, asks user for the file name, and opens the input file to read
     FILE* fptr;
-    fptr = fopen("file.txt", "r");
+    printf("What is the name of the input file would you like to read from: ");
+    scanf("%s", filename);
+    fptr = fopen(filename, "r");
 
     int i = 0;
+    //Loops through each line of the input file, storing the floats in an array
     while(fgets(line, 12, fptr) != NULL) {
         nums[i] = strtof(line, NULL);
         i++;
     }
     fclose(fptr);
 
+    //Runs each statistics function, taking in the sales of each month and the month names as parameters
     printSales(nums, months);
     printSalesReport(nums, months);
     printSixMonths(nums);
