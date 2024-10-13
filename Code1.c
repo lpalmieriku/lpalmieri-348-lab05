76281364eee0a684e47e9a4119cba83f5345e71a
lpalmieriku <l250p415@home.ku.edu>
2024-10-12 23:26:20 -0500

    Finished program draft and testing
    Fixed a few formatting errors and array indexing problems. Need to comment

--- Code1.c
+++ Code1.c
@@ -21,7 +21,7 @@ void printSales(float nums[12], char months[12][10]) {
 }
 
 void printSalesReport(float nums[12], char months[12][10]) {
-    int i = 0, minpos, maxpos;
+    int i = 0, minpos = 0, maxpos = 0;
     float min = nums[0], max = nums[0], average = 0;
     while (i < 12) {
         if (min > nums[i]) {
@@ -43,30 +43,30 @@ void printSixMonths(float nums[12]) {
     printf("\nSix-month moving average report:\n\n");
     for(int i = 0; i <= 6; i++) {
         float average = 0;
-        for(int n = i; n <= i + 6; n++) {
+        for(int n = i; n < i + 6; n++) {
             average += nums[n];
         }
 
         if (i == 0) {
-            printf("January-June\t%.2f\n", average/6);
+            printf("January-June\t$%.2f\n", average/6);
         }
         else if (i == 1) {
-            printf("February-July\t%.2f\n", average/6);
+            printf("February-July\t$%.2f\n", average/6);
         }
         else if (i == 2) {
-            printf("March-August\t%.2f\n", average/6);
+            printf("March-August\t$%.2f\n", average/6);
         }
         else if (i == 3) {
-            printf("April-September\t%.2f\n", average/6);
+            printf("April-September\t$%.2f\n", average/6);
         }
         else if (i == 4) {
-            printf("May-October\t%.2f\n", average/6);
+            printf("May-October\t$%.2f\n", average/6);
         }
         else if (i == 5) {
-            printf("June-November\t%.2f\n", average/6);
+            printf("June-November\t$%.2f\n", average/6);
         }
         else if (i == 6) {
-            printf("July-December\t%.2f\n", average/6);
+            printf("July-December\t$%.2f\n\n", average/6);
         }
     }
 }
@@ -74,7 +74,7 @@ void printSixMonths(float nums[12]) {
 void printHighLow(float nums[12], char months[12][10]) {
     printf("Sales report (highest to lowest)\n\nMonth    \tSales\n");
     int count = 0;
-    while (count < 11) {
+    while (count < 12) {
         float max = nums[0];
         int maxpos = 0;
         for (int x = 0; x < 12; x++) {
@@ -103,9 +103,9 @@ int main() {
     }
     fclose(fptr);
 
-    //printSales(nums, months);
-    //printSalesReport(nums, months);
-    //printSixMonths(nums);
+    printSales(nums, months);
+    printSalesReport(nums, months);
+    printSixMonths(nums);
     printHighLow(nums, months);
 
     return 0;
